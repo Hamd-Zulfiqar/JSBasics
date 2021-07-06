0f13345e0d90bba7de459ceaca54aa8215090c41
Hamd Zulfiqar <hamd.zulfiqar@tintash.com>
2021-07-06 16:26:54 +0500

    JSBasics
    WHAT: Class Blueprint for Todo entity
    
    WHY: So Todo class can be used to add or remove todos from array
    
    IMPACT: HIGH

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//* Todo Model Class
+class Todo {
+    constructor(title, description, date, status){
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.status = status;
+    }
+}
+
+let temp = new Date();
+
+let array = [new Todo("Wake Up","You have to wake up early in morning", temp, false),
+            new Todo("Wash your face","You have to wake up early in morning", temp, false),
+            new Todo("Change Clothes","You have to wake up early in morning", temp, false)];
+
+console.log(array);
